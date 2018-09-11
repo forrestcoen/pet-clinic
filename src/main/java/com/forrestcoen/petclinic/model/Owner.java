a4697f6d483891bcae3f801d47d2d15241bfe6af
Forrest Coen <forrest.coen@gmail.com>
2018-09-10 23:45:51 -0400

    Adding POJO's. Closes #2

---
+++ src/main/java/com/forrestcoen/petclinic/model/Owner.java
@@ -0,0 +1,5 @@
+package com.forrestcoen.petclinic.model;
+
+public class Owner extends Person {
+	
+}
---
+++ src/main/java/com/forrestcoen/petclinic/model/Vet.java
@@ -0,0 +1,5 @@
+package com.forrestcoen.petclinic.model;
+
+public class Vet extends Person {
+
+}
