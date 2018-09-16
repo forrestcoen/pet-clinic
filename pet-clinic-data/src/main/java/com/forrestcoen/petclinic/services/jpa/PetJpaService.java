78ce10f0a8d39e864c171350131c0cafbca8f8ed
Forrest Coen <forrest.coen@gmail.com>
2018-09-16 14:43:18 -0400

    Added PetJpaService. Closes #43.

---
+++ pet-clinic-data/src/main/java/com/forrestcoen/petclinic/services/jpa/PetJpaService.java
@@ -0,0 +1,50 @@
+package com.forrestcoen.petclinic.services.jpa;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import com.forrestcoen.petclinic.model.Pet;
+import com.forrestcoen.petclinic.repositories.PetRepository;
+import com.forrestcoen.petclinic.services.PetService;
+
+@Service
+@Profile("jpa")
+public class PetJpaService implements PetService {
+	private final PetRepository petRepository;
+
+	public PetJpaService(PetRepository petRepository) {
+		this.petRepository = petRepository;
+	}
+
+	@Override
+	public Set<Pet> findAll() {
+		Set<Pet> pets = new HashSet<>();
+		petRepository.findAll().forEach(pets::add);
+
+		return pets;
+	}
+
+	@Override
+	public Pet findById(Long id) {
+		return petRepository.findById(id).orElse(null);
+	}
+
+	@Override
+	public Pet save(Pet pet) {
+		return petRepository.save(pet);
+	}
+
+	@Override
+	public void delete(Pet pet) {
+		petRepository.delete(pet);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		petRepository.deleteById(id);
+	}
+
+}
