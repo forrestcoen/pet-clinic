bb2154c7ee0df037bb2b40b60f64f4879b35a522
Forrest Coen <forrest.coen@gmail.com>
2018-09-16 19:06:50 -0400

    Added Project Lombok to project. Closes #51.

--- pet-clinic-data/src/main/java/com/forrestcoen/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/forrestcoen/petclinic/model/Vet.java
@@ -10,20 +10,28 @@ import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "vets")
 public class Vet extends Person {
 
+	@Builder
+	public Vet(Long id, String firstName, String lastName, Set<Specialty> specialties) {
+		super(id, firstName, lastName);
+		this.specialties = specialties;
+	}
+
 	@ManyToMany(fetch = FetchType.EAGER)
 	@JoinTable(name = "vets_specialties", joinColumns = @JoinColumn(name = "vet_id"), inverseJoinColumns = @JoinColumn(name = "specialty_id"))
 	private Set<Specialty> specialties = new HashSet<>();
-
-	public Set<Specialty> getSpecialties() {
-		return specialties;
-	}
-
-	public void setSpecialties(Set<Specialty> specialties) {
-		this.specialties = specialties;
-	}
 
 }
