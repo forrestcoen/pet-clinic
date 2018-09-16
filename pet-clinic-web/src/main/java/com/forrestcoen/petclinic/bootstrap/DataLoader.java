37c64e3f402bd116eabad1eb4c4fffcc5d170684
Forrest Coen <forrest.coen@gmail.com>
2018-09-16 10:59:54 -0400

    Added PetType to data bootstrapping. Closes #32.

--- pet-clinic-web/src/main/java/com/forrestcoen/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/forrestcoen/petclinic/bootstrap/DataLoader.java
@@ -4,25 +4,35 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import com.forrestcoen.petclinic.model.Owner;
+import com.forrestcoen.petclinic.model.PetType;
 import com.forrestcoen.petclinic.model.Vet;
 import com.forrestcoen.petclinic.services.OwnerService;
+import com.forrestcoen.petclinic.services.PetTypeService;
 import com.forrestcoen.petclinic.services.VetService;
 
 @Component
 public class DataLoader implements CommandLineRunner {
 
 	private final OwnerService ownerService;
-
 	private final VetService vetService;
+	private final PetTypeService petTypeService;
 
-
-	public DataLoader(OwnerService ownerService, VetService vetService) {
+	public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
 		this.ownerService = ownerService;
 		this.vetService = vetService;
+		this.petTypeService = petTypeService;
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
+		PetType dog = new PetType();
+		dog.setName("Dog");
+		PetType savedDogPetType = petTypeService.save(dog);
+		
+		PetType cat = new PetType();
+		dog.setName("Cat");
+		PetType savedCatPetType = petTypeService.save(cat);
+		
 		Owner owner1 = new Owner();
 		
 		owner1.setFirstName("Michael");
